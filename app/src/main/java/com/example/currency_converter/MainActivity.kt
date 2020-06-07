290f33deecaac5c2a340d5015d25d443c3e3a81f
ArtemiyKotsyubenko <kotsiubenko.aa@phystech.edu>
2020-06-07 15:39:25 +0300

    inheritance fixer

--- app/src/main/java/com/example/currency_converter/MainActivity.kt
+++ app/src/main/java/com/example/currency_converter/MainActivity.kt
@@ -3,94 +3,138 @@ package com.example.currency_converter
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.FileUriExposedException
 import android.view.View
 import android.widget.*
 import khttp.responses.Response
 import kotlinx.android.synthetic.main.activity_main.*
-
 import kotlinx.android.synthetic.main.activity_main.spinnerFrom
 import org.json.JSONObject
 import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
-
-    fun getApi(){
-        val response: Response = khttp.get(
-            url = "https://api.exchangeratesapi.io/latest",
-            params = mapOf(
-                "symbols" to "$spinnerTo.selectedItem",  //должно работать, но это не точно
-                "base" to "$spinnerFrom.selectedItem"))
-
-        val obj: JSONObject = response.jsonObject
-        var textRes:TextView? = null
-        textRes=  findViewById(R.id.textResult)
-        textRes?.text = "${obj["rates"]}"
-    }
-
-    private fun setUpSpinner() {
-        val adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.values_list,
-            android.R.layout.simple_spinner_item)
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerFrom.adapter = adapter
-        //spinnerFrom.onItemSelectedListener = onItemSelectedListener0 // код ниже,выводит в графу - надо ли?
-        spinnerTo.adapter = adapter
-        //spinnerTo.onItemSelectedListener = onItemSelectedListener1//дубль два
-    }
+public class MainActivity : AppCompatActivity() {
+    private var second :String = "USD"
+    private var first: String = "EUR"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         var currencyList: Array<String> =
             resources.getStringArray(R.array.values_list) // обьявляем массив с названиями валют
-        setUpSpinner()                        // создаем адаптер для спиннера -  вызов функции
+
+        fun setUpSpinner() {
+            val adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.values_list,
+                android.R.layout.simple_spinner_item
+            )
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerFrom.adapter = adapter
+            spinnerTo.adapter = adapter
+
+            spinnerFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    first = spinnerFrom.selectedItem.toString()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    //var first: String = "EUR"
+                    Toast.makeText(
+                        this@MainActivity,
+                        "ничего не выбрано, первая валюта - евро",
+                        Toast.LENGTH_LONG
+                    )
+                }
+            }
+
+            spinnerTo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    second = spinnerTo.selectedItem.toString()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    //var first: String = "USD"
+                    Toast.makeText(
+                        this@MainActivity,
+                        "ничего не выбрано, вторая валюта - доллар",
+                        Toast.LENGTH_LONG
+                    )
+                }
+
+            }
+
+        }
+
+
+
 
         buttonConv.setOnClickListener {
             //функции вызываемые нажатием на кнопку
             fun onClick(p: View?) {
-                getApi()                    // делаем запрос
-                //button_convert()           // получаем циферки из запроса
-                //counting()                // считаем циферки
-                val task = CurrencyAsynk()
+                setUpSpinner()
+                val task = CurrencyTask(this)
                 task.execute()
-
             }
         }
     }
-}
 
-    class CurrencyAsynk() : AsyncTask<Void, Void, String>() {
-        override fun doInBackground(vararg params: Void?): String? {
-            // ...
-            return null;
-        }
+
+    class CurrencyTask constructor(private var activity: MainActivity) :
+        AsyncTask<MainActivity, Void, Void>() {
 
         override fun onPreExecute() {
             super.onPreExecute()
-            // ...
+
+            val response: Response = khttp.get(
+                url = "https://api.exchangeratesapi.io/latest",
+                params = mapOf(
+                    "symbols" to "${activity.second}",  //должно работать, но это не точно
+                    "base" to "${activity.first}"
+                )
+            )
+            val obj: JSONObject = response.jsonObject
+            //var valueName = "${obj["rates"]}" получаем название и курс
+            val nameValue: JSONObject = obj.getJSONObject("rates")
+            val valueStr: String = nameValue.getString(activity.second)  //получаем курс
+            val rate: Double = valueStr.toDouble()
+            val input: EditText = activity.findViewById(R.id.input)
+            //var result: Double = (input * rate).toString()
+            //return result
         }
 
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            // ...
+
+//        override fun onPostExecute(result: String?) {
+//            super.onPostExecute(result)
+//            var textResult: TextView = findViewById(R.id.textResult)
+//            textResult.text = result
+//        }
+
+
+        override fun doInBackground(vararg params: MainActivity?): Void {
+            TODO("Not yet implemented")
         }
     }
 
 
+}
 
 
-
-
-
-
-
-
-
-
-      //  Toast.makeText(
-           // this, "Spinner 1 " + spinnerFrom.selectedItem.toString() +
-           //       "\nSpinner 2 " + spinnerTo.selectedItem.toString(), Toast.LENGTH_LONG) }
+//  Toast.makeText(
+// this, "Spinner 1 " + spinnerFrom.selectedItem.toString() +
+//       "\nSpinner 2 " + spinnerTo.selectedItem.toString(), Toast.LENGTH_LONG) }
